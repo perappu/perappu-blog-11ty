2b165dee245d8a9f47193cdcb3529a60ce93fdaa
Kazimir <stokoriel@gmail.com>
2025-08-02 15:18:37 -0500

    shuffle the files around, maybe that will make vercel happy

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -12,13 +12,12 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default async function (eleventyConfig) {
 
-
 	const postcssFilter = (cssCode, done) => {
 		// we call PostCSS here.
 		postCss([tailwind(import('./tailwind.config')), autoprefixer(), cssnano({ preset: 'default' })])
 			.process(cssCode, {
 				// path to our CSS file
-				from: './_includes/assets/tailwind.css'
+				from: './src/_includes/assets/tailwind.css'
 			})
 			.then(
 				(r) => done(null, r.css),
@@ -38,19 +37,21 @@ export default async function (eleventyConfig) {
 	eleventyConfig.addPlugin(tocPlugin, { ul: "false" });
 
 	eleventyConfig.setLibrary("md", markdownit({ html: true }).use(anchor).use(markdownItAttrs, {
-  // optional, these are default options
-  leftDelimiter: '{',
-  rightDelimiter: '}',
-  allowedAttributes: []  // empty array = all attributes are allowed
-}));
+		// optional, these are default options
+		leftDelimiter: '{',
+		rightDelimiter: '}',
+		allowedAttributes: []  // empty array = all attributes are allowed
+	}));
+
+	eleventyConfig.setInputDirectory("_src");
 
 	eleventyConfig.addPassthroughCopy({ './_includes/assets/content/img/**.*': '/assets/content/img/' });
 	eleventyConfig.addPassthroughCopy({ './content/content/img/**/**.*': '/content/img/' });
 	eleventyConfig.addPassthroughCopy({ "./_includes/assets/favicon.ico": "/" });
 	eleventyConfig.addPassthroughCopy({ "./_includes/assets/fslightbox.js": "/" });
 
-	eleventyConfig.addWatchTarget('./_includes/assets/tailwind.css');
-	eleventyConfig.addWatchTarget('./content/**/*.*');
+	eleventyConfig.addWatchTarget('/_includes/assets/tailwind.css');
+	eleventyConfig.addWatchTarget('/content/**/*.*');
 
 	eleventyConfig.addNunjucksAsyncFilter('postcss', postcssFilter);
 	eleventyConfig.setLayoutsDirectory("_includes/layouts");
